d79066f74ed1e9faa257ecdc1ce618ee53d47f6e
Rebs20 <louismariana24@gmail.com>
2021-10-20 01:25:41 +0100

    added footer

---
+++ Script/footer.js
@@ -0,0 +1,21 @@
+const html = `
+<div class="container py-3 mt-sm-5">
+        <div class="row justify-content-sm-between">
+          <div class="col-sm-6">
+            <h3 class="text-white">About</h3>
+            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sit nemo voluptatem alias, commodi quaerat eligendi, amet aperiam itaque molestiae magni blanditiis. Hic totam facere enim ipsa iusto nostrum voluptatem!
+            Minima nobis repellat id a dicta? Aliquid, laboriosam doloremque ullam reiciendis error nisi nobis consequuntur qui voluptatibus doloribus enim quidem iste voluptate eaque fugit, perspiciatis odio, expedita rem cum suscipit.</p>
+          </div>
+
+          <div class="col-sm-5 mt-3 mt-sm-0">
+            <h3 class='text-white'>Contacts</h3>
+            <ul class="text-white ms-0 mx-0 px-0">
+              <li>Tel - (+234) 81000 pet store</li>
+              <li>Email - emmanuelrebecca132@gmail.com</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      `;
+
+      document.querySelector("footer").innerHTML = html;
